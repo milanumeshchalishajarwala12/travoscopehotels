8fd08587a8b52e72bfecec0caa6e0cee0765a067
milanchalishajarwala12 <MilanChalishajarwala@KALPESHs-MacBook-Pro-2.local>
2020-04-22 13:15:42 -0400

    Configuration to deploy to heroku

--- Server.js
+++ Server.js
@@ -15,15 +15,6 @@ app.use('/api/auth', require('./Routes/api/auth'));
 app.use('/api/staff', require('./Routes/api/staff'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
