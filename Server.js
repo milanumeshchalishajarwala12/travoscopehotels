aa94a9cfa8baf5f545dc8cb17671db372a5c4b5b
milanchalishajarwala12 <MilanChalishajarwala@KALPESHs-MacBook-Pro-2.local>
2020-04-22 00:45:39 -0400

    Prepared for deployment

--- Server.js
+++ Server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
 //INIT middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json('Admin app is running');
-});
-
 app.use('/api/rooms', require('./Routes/api/rooms'));
 app.use('/api/users', require('./Routes/api/users'));
 app.use('/api/admin', require('./Routes/api/admin'));
 app.use('/api/auth', require('./Routes/api/auth'));
 app.use('/api/staff', require('./Routes/api/staff'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
