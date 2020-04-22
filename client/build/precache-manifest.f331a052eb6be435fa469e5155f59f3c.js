e454899fd085ca31164b463f932e53bf3dfe7283
milanchalishajarwala12 <MilanChalishajarwala@KALPESHs-MacBook-Pro-2.local>
2020-04-22 00:59:17 -0400

    Production

---
+++ client/build/precache-manifest.f331a052eb6be435fa469e5155f59f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac00441bbeec831713a375d6b258282c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d52dd5bc717c53513b6",
+    "url": "/static/css/main.b4b1881b.chunk.css"
+  },
+  {
+    "revision": "2d55f0ba55c141da10c0",
+    "url": "/static/js/2.59ea4545.chunk.js"
+  },
+  {
+    "revision": "b3b6c1cbbc233b363676fc8c08cfb0ca",
+    "url": "/static/js/2.59ea4545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d52dd5bc717c53513b6",
+    "url": "/static/js/main.df14750c.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
